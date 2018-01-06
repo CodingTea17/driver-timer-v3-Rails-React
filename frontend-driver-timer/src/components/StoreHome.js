1b131a05ba6d3c9ed0b62014c8f1909a66414f92
Dawson M <dawson.mortenson@gmail.com>
2018-01-05 19:46:20 -0800

    begin ui integration

--- frontend-driver-timer/src/components/StoreHome.js
+++ frontend-driver-timer/src/components/StoreHome.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Sidebar, Menu, Segment, Grid, Button, Header, Modal } from 'semantic-ui-react'
+
 import Driver from './Driver';
 
 // const driverStyle = {
@@ -10,6 +11,7 @@ class StoreHome extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      visible: false,
       drivers: [],
       store_number: this.props.match.params.id
 
@@ -24,29 +26,55 @@ class StoreHome extends Component {
     })
   }
 
+  toggleVisibility = () => this.setState({ visible: !this.state.visible });
+
   render() {
+    const { visible } = this.state;
+
     return (
       <div style={ {textAlign: "center"} }>
-        <div style={ {backgroundColor: '#b71c1c', height: '4em', color: 'white', marginBottom: '2em'} }>
+        <div style={ {backgroundColor: '#b71c1c', height: '4em', color: 'white'} }>
           <p style={ {height: '100%', fontSize: '3em', fontWeight: '800', paddingBottom: '0.5px'} }>Your Drivers</p>
         </div>
-        <Grid container columns={4}>
-          <Grid.Row>
-            {
-              // Make sure there are any drivers for the store
-              // TODO: It would be cool to render an example driver or something if a store has no drivers
-              (this.state.drivers && this.state.drivers.length) && this.state.drivers.map((driver, index) => (
-                <Grid.Column key={driver.id}>
-                  <Driver
-                    driver={driver}
-                    storeNumber={this.state.store_number}
-                    count={index + 1}
-                  />
-                </Grid.Column>
-              ))
-            }
-          </Grid.Row>
-        </Grid>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} animation='scale down' width='thin' visible={ visible } icon='labeled' vertical inverted>
+            <Menu.Item name='newdriver'>
+              <Modal trigger={<Button>Add Driver</Button>}>
+                <Modal.Header>Add a New Driver</Modal.Header>
+                <Modal.Content>
+                  <Modal.Description>
+                    <p>I am a new driver form</p>
+                  </Modal.Description>
+                </Modal.Content>
+              </Modal>
+            </Menu.Item>
+            <Menu.Item name='driverlist'>
+              List of current drivers
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Button onClick={this.toggleVisibility}>Toggle Visibility</Button>
+              <Grid columns={4}>
+                <Grid.Row>
+                  {
+                    // Make sure there are any drivers for the store
+                    // TODO: It would be cool to render an example driver or something if a store has no drivers
+                    (this.state.drivers && this.state.drivers.length) && this.state.drivers.map((driver, index) => (
+                      <Grid.Column key={driver.id}>
+                        <Driver
+                          driver={driver}
+                          storeNumber={this.state.store_number}
+                          count={index + 1}
+                        />
+                      </Grid.Column>
+                    ))
+                  }
+                </Grid.Row>
+              </Grid>
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
     );
   }
